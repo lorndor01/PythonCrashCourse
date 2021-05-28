2275a3813a1387a0cbc762157b4194edb077da22
Liam Orndorff <liam.orndorff@hope.edu>
2021-05-27 20:02:57 -0400

    Initialized pygame environment and window

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,31 @@
+import sys
+import pygame
+from settings import Settings
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #Make the most recently drawn screen visible.
+            self.screen.fill(self.settings.bg_color)
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make game instance and run the game.
+    alien_invasion = AlienInvasion()
+    alien_invasion.run_game()
