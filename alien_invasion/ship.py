8bc1146587357484f20be3e64458e7abd3be7534
Liam Orndorff <liam.orndorff@hope.edu>
2021-05-27 23:26:20 -0400

    Ship can not change its horizontal position.

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -6,6 +6,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         #Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,23 @@ class Ship:
         #Start each new ship at bottom center of the screen.
         self.rect.midbottom =self.screen_rect.midbottom
 
+        # Movement flags
+        self.moving_left = False
+        self.moving_right = False
+
+        # Keep tracking of positions that are fractions of pixels.
+        self.x = float(self.rect.x)
+
+
+    def update(self):
+        """Update the ship's position based on movement flags."""
+        if (self.moving_right == True) and (not self.rect.right == self.screen_rect.right):
+            self.x += self.settings.ship_speed
+        if self.moving_left == True and (self.rect.left >0):
+            self.x -= self.settings.ship_speed
+
+        #Update ship's rect position.
+        self.rect.x = self.x
     def blitme(self):
         """Draw the ship at its current position."""
         self.screen.blit(self.image, self.rect)
