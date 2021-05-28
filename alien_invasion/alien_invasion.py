6ba3691628e0ee32432ff0d585ebde81b4a7b4d5
Liam Orndorff <liam.orndorff@hope.edu>
2021-05-28 18:43:56 -0400

    Created class to represent and draw bullet object

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
@@ -25,14 +26,20 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
+        self.bullets = pygame.sprite.Group()
+
     def run_game(self):
         """Main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
+
             self.ship.update()
+            self._update_bullets()
+
             #Make the most recently drawn screen visible.
             self._update_screen()
+
 
     def _check_events(self):
         """Respond to key presses and mouse events."""
@@ -54,6 +61,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         if event.key == pygame.K_q:
             sys.exit()
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _handle_key_up(self, event):
         """Handle key up events."""
@@ -66,8 +75,24 @@ class AlienInvasion:
         """Update images on the screen and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw()
         pygame.display.flip()
 
+    def _fire_bullet(self):
+        """Create a bullet and add it to the bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        #Update bullets location
+        self.bullets.update()
+        
+        #Get rid of bullets that have disappearerd.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
 if __name__ == '__main__':
     # Make game instance and run the game.
