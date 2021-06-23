dd661465cc9cdef4b4474582ee57acef81a7391d
Liam Orndorff <liam.orndorff@hope.edu>
2021-06-23 16:27:13 -0400

    Added settings pertaining to score keepint

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -23,6 +23,8 @@ class Settings:
         #How quickly the game speeds up
         self.speedup_scale = 1.1
 
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -34,11 +36,16 @@ class Settings:
         #Fleet direction of 1 represents right and -1 represents left.
         self.fleet_direction = 1
 
+        #Scoring
+        self.alien_points = 50
+
     def increase_speed(self):
         """Increase speed settings"""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
 
 
 
