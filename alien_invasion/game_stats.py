bc9154f8f83522311d3c4eb4206074f2b31e61db
Liam Orndorff <liam.orndorff@hope.edu>
2021-06-23 18:44:15 -0400

    Class now interacts with high score json to keep track of high score

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -1,3 +1,4 @@
+import json
 class GameStats:
     """Track statistics for alien invasion"""
 
@@ -8,11 +9,13 @@ class GameStats:
 
         #Start in an inactive state
         self.game_active = False
+        with open(ai_game.high_score_file) as f:
+            self.high_score = json.load(f)['score']
 
 
     def reset_stats(self):
         """Initialize statistics that can change during a game"""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
 
-    
