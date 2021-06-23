165bbc0f27ca2eb1f4099e1ee2ac9a42c9658c69
Liam Orndorff <liam.orndorff@hope.edu>
2021-06-23 18:44:34 -0400

    Gameboard now displays who holds the high score

--- alien_invasion/score_board.py
+++ alien_invasion/score_board.py
@@ -1,10 +1,12 @@
 import pygame
-
+from pygame.sprite import Group
+from ship import Ship
 class ScoreBoard:
     """A class to report scoring information"""
 
     def __init__(self, ai_game):
         """Initialize scorekeeping attributes"""
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -16,6 +18,9 @@ class ScoreBoard:
 
         #Prepare the initial score image
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         rounded_score = round(self.stats.score, -1)
@@ -28,6 +33,50 @@ class ScoreBoard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Turn the high score into a rendered image"""
+        high_score = self.stats.high_score
+        high_score_rounded = round(high_score, -1)
+        high_score_str = "{:,}".format(high_score_rounded)
+        high_score_str = f"High Score: {self.ai_game.high_score_holder} - " + high_score_str
+        self.high_score_image = self.font.render(high_score_str, True,
+            self.text_color, self.settings.bg_color)
+
+        #Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def prep_level(self):
+        """Turn the level into a rendered image"""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color,
+            self.settings.bg_color)
+
+        #Position the level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        """Show how many ships are left"""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+
     def show_score(self):
         """Draw score to screen"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
+
+    def check_high_score(self):
+        """Check to see if there is a new high score"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.ai_game.high_score_holder = self.ai_game.session_name
+            self.prep_high_score()
