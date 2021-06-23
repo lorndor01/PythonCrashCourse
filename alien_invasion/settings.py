beeb7326c32e0e35dd34c0fdb172d3aeebaede6e
Liam Orndorff <liam.orndorff@hope.edu>
2021-06-23 15:57:02 -0400

    Seperated dynamic and static settings and added speed-up constance

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -2,25 +2,44 @@ class Settings:
     """A class to store all the settings for the game Alien Invasion"""
 
     def __init__(self):
-        """Initialize the game's sfettings."""
+        """Initialize the game's static settings."""
         # Screen settings.
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
-        self.ship_speed = 1.5
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+
+        #Ship settings
+        self.ship_limit = 2
+
+        #Bullet settings
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 6
 
-        # Alien settings
-        self.alien_speed = .5
-        self.fleet_drop_speed = 25
-        # fleet_direction of 1 represents right; -1 represents left.
+        #Alien settings
+        self.fleet_drop_speed = 10
+
+        #How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.0
+        self.alien_speed = .75
+
+        #Fleet direction of 1 represents right and -1 represents left.
         self.fleet_direction = 1
 
-        #Statistics
-        self.ship_limit = 2
+    def increase_speed(self):
+        """Increase speed settings"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+
 
         
