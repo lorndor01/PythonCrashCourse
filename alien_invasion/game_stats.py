4d77e57c732c57553f2e77004bf2de701df297e7
Liam Orndorff <liam.orndorff@hope.edu>
2021-06-23 16:27:23 -0400

    Added score attribute

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -9,6 +9,10 @@ class GameStats:
         #Start in an inactive state
         self.game_active = False
 
+
     def reset_stats(self):
         """Initialize statistics that can change during a game"""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
+
+    
